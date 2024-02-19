b7e4e7517a5066b5796c574d1bbc5811a22d6863
Bruno Nascimento <46034952+BrunoASNascimento@users.noreply.github.com>
2024-02-19 23:29:35 +0000

    22, 25, 26 okay

--- 22-racionaisStruct.c
+++ 22-racionaisStruct.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct tRacional racional(int, int); 
+struct tRacional racional(int, int);
 struct tRacional negativo(struct tRacional);
 struct tRacional soma(struct tRacional, struct tRacional);
 struct tRacional multiplica(struct tRacional, struct tRacional);
@@ -12,43 +12,50 @@ struct tRacional
 {
     int n;
     int d;
-}; 
+};
 
-struct tRacional racional(int n, int d) {
+struct tRacional racional(int n, int d)
+{
     struct tRacional r;
     r.n = n;
     r.d = d;
     return r;
 }
 
-struct tRacional soma(struct tRacional r1, struct tRacional r2) {
+struct tRacional soma(struct tRacional r1, struct tRacional r2)
+{
     struct tRacional result;
-    result.n = r1.n * r2.d + r2.n * r1.d; 
-    result.d = r1.d * r2.d; 
+    result.n = r1.n * r2.d + r2.n * r1.d;
+    result.d = r1.d * r2.d;
     return result;
 }
 
-struct tRacional multiplica(struct tRacional r1, struct tRacional r2) {
+struct tRacional multiplica(struct tRacional r1, struct tRacional r2)
+{
     struct tRacional result;
-    result.n = r1.n *  r2.n ; 
-    result.d = r1.d * r2.d; 
+    result.n = r1.n * r2.n;
+    result.d = r1.d * r2.d;
     return result;
 }
 
-struct tRacional divide(struct tRacional r1, struct tRacional r2) {
+struct tRacional divide(struct tRacional r1, struct tRacional r2)
+{
     struct tRacional result;
-    result.n = r1.n * r2.d ; 
-    result.d = r1.d * r2.n; 
+    result.n = r1.n * r2.d;
+    result.d = r1.d * r2.n;
     return result;
 }
-struct tRacional negativo(struct tRacional r) {
+struct tRacional negativo(struct tRacional r)
+{
     struct tRacional result;
-    result.n = -r.n; 
-    result.d = r.d; 
+    result.n = -r.n;
+    result.d = r.d;
     return result;
 }
-int gcd(int a, int b) {
-    while (b != 0) {
+int gcd(int a, int b)
+{
+    while (b != 0)
+    {
         int t = b;
         b = a % b;
         a = t;
@@ -56,12 +63,14 @@ int gcd(int a, int b) {
     return a;
 }
 
-struct tRacional reduz(struct tRacional r) {
+struct tRacional reduz(struct tRacional r)
+{
     int g = gcd(abs(r.n), abs(r.d));
     struct tRacional result;
     result.n = r.n / g;
     result.d = r.d / g;
-    if (result.d < 0) {
+    if (result.d < 0)
+    {
         result.n = -result.n;
         result.d = -result.d;
     }
@@ -69,30 +78,29 @@ struct tRacional reduz(struct tRacional r) {
 }
 int main()
 {
-    
+
     int n1, d1, n2, d2;
     char op;
-    while (scanf("%d %d %c %d %d", &n1, &d1, &op, &n2, &d2) != EOF)
+    scanf("%d %d %c %d %d", &n1, &d1, &op, &n2, &d2);
+
+    struct tRacional r1 = racional(n1, d1);
+    struct tRacional r2 = racional(n2, d2);
+    struct tRacional result;
+    switch (op)
     {
-        struct tRacional r1 = racional(n1, d1);
-        struct tRacional r2 = racional(n2, d2);
-        struct tRacional result;
-        switch (op)
-        {
-        case '+':
-            result = soma(r1, r2);
-            break;
-        case '-':
-            result = soma(r1, negativo(r2));
-            break;
-        case '*':
-            result = multiplica(r1, r2);
-            break;
-        case '/':
-            result = divide(r1, r2);
-            break;
-        }
-        result = reduz(result);
-        printf("%d %d\n", result.n, result.d);
+    case '+':
+        result = soma(r1, r2);
+        break;
+    case '-':
+        result = soma(r1, negativo(r2));
+        break;
+    case '*':
+        result = multiplica(r1, r2);
+        break;
+    case '/':
+        result = divide(r1, r2);
+        break;
     }
+    result = reduz(result);
+    printf("%d %d\n", result.n, result.d);
 }
---
+++ 26-diaAnteriorStruct.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+typedef struct tData
+{
+    int dia, mes, ano;
+} Data;
+
+int isBissexto(int ano);
+// int isValida(Data dt);
+
+int isBissexto(int ano)
+{
+    if (ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+Data isValida(Data dt)
+{
+    int bissexto = isBissexto(dt.ano);
+
+    if (dt.dia == 0)
+    {
+        dt.mes--;
+    }
+
+    if (dt.mes == 2)
+    {
+        if (bissexto)
+        {
+            if (dt.dia < 1 || dt.dia > 29)
+            {
+                dt.dia = 29;
+            }
+        }
+        else
+        {
+            if (dt.dia < 1 || dt.dia > 28)
+            {
+                dt.dia = 28;
+            }
+        }
+    }
+    if (dt.mes == 4 || dt.mes == 6 || dt.mes == 9 || dt.mes == 11)
+    {
+        if (dt.dia < 1 || dt.dia > 30)
+        {
+            dt.dia = 30;
+        }
+    }
+
+    if (dt.dia < 1 || dt.dia > 31)
+    {
+        dt.dia = 31;
+    }
+
+    if (dt.mes < 1 || dt.mes > 12)
+    {
+        dt.mes = 12;
+        dt.ano--;
+    }
+    return dt;
+}
+
+int main()
+{
+    Data dt;
+    int bissexto;
+
+    int returnValue = scanf("%d %d %d", &dt.dia, &dt.mes, &dt.ano);
+    dt.dia--;
+    dt = isValida(dt);
+
+    printf("%d %d %d\n", dt.dia, dt.mes, dt.ano);
+}
